9817a191df11f324bab858f1c4d7ff70cd59dbe2
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-06-18 23:26:13 +0100

    fix rubocop errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,30 +7,29 @@ class UsersController < ApplicationController
     @user = User.new[user_params]
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-  if @user.save
-    redirect_to new_user_path
-  else
-    render :new
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
-
-def edit
-  @user = User.find(params[:id])
-end
-
-def update
-  @user = User.find(params[:id])
-  if @user.update(user_params)
-    redirect_to edit_user_path(@user)
-  else
-    render 'edit'
-  end
-end
-
-private
-def user_params
-  params.require(:user).permit(:username, :email, :password)
-end
-end
-
-  
